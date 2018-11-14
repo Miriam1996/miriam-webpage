da19051879513bac7c37adf1daab7f91fe4beb4c
Miriam1996 <miriamschnoll1996@gmail.com>
2018-11-13 22:59:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 $(document).ready(function(){
- 	$('#submit').click(function() {
-   console.log("clicked");
-   console.log($('#selection').val());
-  
-  // go to page of projects
- 		if($('#selection').val() == 1){
-      document.location.href="./miriam%20schnoll's%20Java.html";
-      }
-      else if($('#selection').val() == 2){
-        document.location.href="./miriam%20schnoll's%20c++%20project.html";
-      }
-      
+ 	$('#nav-placeholder').load("nav.html");
+   
+       
 
- 	});
+
+
+
 
 });
