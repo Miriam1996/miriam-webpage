66ac04a7d34ebf33d7e1db2d94ffd61b26e9562b
Miriam1996 <miriamschnoll1996@gmail.com>
2018-11-14 00:31:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-
- $(document).ready(function(){
+$(document).ready(function(){
   $("#menu li").hover(function(){
     $(".dropdown-menu", this).slideDown(100);
   }, function(){
     $(".dropdown-menu", this).stop().slideUp(100);
   });
 })
-});
        
 
 
